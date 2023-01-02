76ab09f1f8c3392ec721d95098f1df578e645163
Clara Charriau <charriau.clara@gmail.com>
2023-01-02 12:08:54 +0100

    [Cart] Création de la page panier et ses composants

---
+++ src/app/components/cart/cart-all-products/cart-all-products.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cart-all-products',
+  templateUrl: './cart-all-products.component.html',
+  styleUrls: ['./cart-all-products.component.css']
+})
+export class CartAllProductsComponent {
+
+}
---
+++ src/app/components/cart/cart-counter/cart-counter.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cart-counter',
+  templateUrl: './cart-counter.component.html',
+  styleUrls: ['./cart-counter.component.css']
+})
+export class CartCounterComponent {
+
+}
