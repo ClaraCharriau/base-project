d559c5121441f704fc2209aa407b78d2b6893a9f
Clara Charriau <charriau.clara@gmail.com>
2023-01-03 17:16:35 +0100

    creation méthode du ProductsService

--- src/app/pages/single-category/single-category.component.ts
+++ src/app/pages/single-category/single-category.component.ts
@@ -1,8 +1,11 @@
 import { Component, Input } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductsService } from 'src/app/service/products/products.service';
+import { Product } from 'src/mocks/product-list.mock';
 
 export interface Link {
-  name : string;
-  favicon : string;
+  name: string;
+  favicon: string;
 }
 @Component({
   selector: 'app-single-category',
@@ -12,4 +15,26 @@ export interface Link {
 export class SingleCategoryComponent {
   @Input() links!: Link[];
 
-}
+  product?: Product;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private productService: ProductsService,
+  ) { }
+
+  ngOnInit() {
+    this.getProductByCategory();
+  }
+
+  getProductByCategory() {
+    const category = this.route.snapshot.paramMap.get('category');
+    const foundProduct = this.productService.getProductByCategory(category);
+
+    if (foundProduct) {
+      this.product = foundProduct;
+    } else {
+      this.router.navigate(['/']);
+    }
+
+  }
