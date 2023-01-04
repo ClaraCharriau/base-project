a64429d9ac5e69d404c9162a378b0cc2daa0bfd6
Clara Charriau <charriau.clara@gmail.com>
2023-01-04 13:33:48 +0100

    début navigation vers détails produits

--- src/app/pages/single-product/single-product.component.ts
+++ src/app/pages/single-product/single-product.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductsService } from 'src/app/service/products/products.service';
+import { Product } from 'src/mocks/product-list.mock';
 
 @Component({
   selector: 'app-single-product',
@@ -6,5 +9,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./single-product.component.css']
 })
 export class SingleProductComponent {
+
+  product?: Product;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private productService: ProductsService,
+  ) { }
+
+  ngOnInit() {
+    this.getProductById();
+  }
+
+  getProductById() {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    const foundProduct = this.productService.getProductById(id);
+
+    if (foundProduct) {
+      this.product = foundProduct;
+    } else {
+      this.router.navigate(['/']);
+    }
+
+  }
 
 };
