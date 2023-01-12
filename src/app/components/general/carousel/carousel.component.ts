9a0a0538360b06c158313ad0ada435effbe8f092
Clara Charriau <charriau.clara@gmail.com>
2023-01-12 17:09:43 +0100

    update work

--- src/app/components/general/carousel/carousel.component.ts
+++ src/app/components/general/carousel/carousel.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-carousel',
@@ -6,6 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./carousel.component.css']
 })
 export class CarouselComponent {
+
+  // Récupérer le contenant du carousel...
+  // carouselSlider = this.document.querySelector('.cache-carousel-two') as HTMLElement;
+  //carouselSlider: HTMLElement = this.document.getElementById('cache-carousel-two');
+
+
+  @ViewChild('cache', { static: false }) carouselSlider!: HTMLDivElement;
+
+  ngAfterViewInit() {
+    this.carouselSlider;
+  }
+
+// Prendre la longueur d'un élément du carousel
+itemWidth: number = 150;
+
+slideToLeft() {
+  this.carouselSlider.scrollBy({
+    left: this.itemWidth,
+    behavior: 'smooth',
+  });
+}
+
+slideToRight() {
+  this.carouselSlider.scrollBy({
+    left: -this.itemWidth,
+    behavior: 'smooth',
+  });
+}
 
 
 
